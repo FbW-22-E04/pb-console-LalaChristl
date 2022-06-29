18114f8104c2f4609d2a83fb75cbcbaeb9d4bf39
LalaChristl <lesleyannchristl@gmail.com>
2022-06-29 20:02:39 +0200

    Retry exercise

--- solution.js
+++ solution.js
@@ -1,19 +1,18 @@
 // write your code in this file
-
 console.log("This is and exercise in console logging");
-
-const exercise = "this is the value";
+const exercise = "This is the value";
 console.log(exercise);
-
 const firstName = "Lala";
 const lastName = "Christl";
 
 console.log(firstName, lastName);
 
-const city = 'Berlin';
-let job = 'Web Dev Student';
-let age = '38';
+let city = "Berlin";
+let job = "Web Dev Student";
+let age = "38";
 
-console.log(firstName, lastName, city, job, age)
+console.log(firstName, lastName, city, job, age);
 
-console.log(firstName, lastName + " is " + age + " and lives in " + city + " and works as a " + job);
+console.log(firstName, lastName + " is a " + age, job + " who lives in " + city)
+
+
