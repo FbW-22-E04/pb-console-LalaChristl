495e1a670e5c477afb810e31ef4cd39328616c96
LalaChristl <lesleyannchristl@gmail.com>
2022-06-29 14:34:58 +0200

    added my work to solutions.js

--- solution.js
+++ solution.js
@@ -1,1 +1,19 @@
 // write your code in this file
+
+console.log("This is and exercise in console logging");
+
+const exercise = "this is the value";
+console.log(exercise);
+
+const firstName = "Lala";
+const lastName = "Christl";
+
+console.log(firstName, lastName);
+
+const city = 'Berlin';
+let job = 'Web Dev Student';
+let age = '38';
+
+console.log(firstName, lastName, city, job, age)
+
+console.log(firstName, lastName + " is " + age + " and lives in " + city + " and works as a " + job);
